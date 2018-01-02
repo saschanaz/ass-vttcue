b63f5b74d2749c71de47fa026ba0026ddbe8367f
Kagami Sascha Rosylight <saschanaz@outlook.com>
2018-01-02 16:51:36 +0900

    temporarily use forked branch

--- sources/index.ts
+++ sources/index.ts
@@ -1,4 +1,5 @@
-import parseASS = require("ass-parser");
+import parseASS from "ass-parser";
+import * as ASSParser from "ass-parser";
 
 main();
 
@@ -11,7 +12,7 @@ function main() {
     for (const section of ass) {
         if (section.section === "Script Info") {
             for (const item of section.body) {
-                if ("type" in item && item.type === "comment") {
+                if (isASSComment(item)) {
                     info.push({ type: "note", value: item.value });
                 }
                 else {
@@ -21,7 +22,7 @@ function main() {
         }
         else if (section.section === "V4 Styles") {
             for (const item of section.body) {
-                if (item.type === "comment") {
+                if (isASSComment(item)) {
                     info.push({ type: "note", value: item.value });
                 }
                 else if (item.key !== "Format") {
@@ -31,7 +32,7 @@ function main() {
         }
         else if (section.section === "Events") {
             for (const item of section.body) {
-                if (item.type === "comment") {
+                if (isASSComment(item)) {
                     info.push({ type: "note", value: item.value });
                 }
                 else if (item.key !== "Format") {
@@ -53,4 +54,8 @@ interface WebVTTNote {
 interface WebVTTStyle {
     type: "style";
     value: string;
+}
+
+function isASSComment(obj: ASSParser.ASSCommentItem | ASSParser.ASSSectionBodyFormatItem | ASSParser.ASSSectionBodyStringItem | ASSParser.ASSSectionBodyStringDictionaryItem): obj is ASSParser.ASSCommentItem {
+    return "type" in obj && (obj as any).type === "comment";
 }
--- sources/module.d.ts
+++ sources/module.d.ts
@@ -1,32 +1,32 @@
 declare module "ass-parser" {
     function parseASS(text: string, options?: { comments?: boolean }): (ASSScriptInfoSection | ASSScriptV4StylesSection | ASSEventsSection)[];
-    export = parseASS;
+    export default parseASS;
 
-    interface ASSScriptInfoSection {
+    export interface ASSScriptInfoSection {
         section: "Script Info";
         body: (ASSSectionBodyStringItem | ASSCommentItem)[];
     }
-    interface ASSScriptV4StylesSection {
+    export interface ASSScriptV4StylesSection {
         section: "V4 Styles";
         body: (ASSSectionBodyFormatItem | ASSSectionBodyStringDictionaryItem | ASSCommentItem)[];
     }
-    interface ASSEventsSection {
+    export interface ASSEventsSection {
         section: "Events";
         body: (ASSSectionBodyFormatItem | ASSSectionBodyStringDictionaryItem | ASSCommentItem)[];
     }
-    interface ASSCommentItem {
+    export interface ASSCommentItem {
         type: string;
         value: string;
     }
-    interface ASSSectionBodyStringItem {
+    export interface ASSSectionBodyStringItem {
         key: string;
         value: string;
     }
-    interface ASSSectionBodyFormatItem {
+    export interface ASSSectionBodyFormatItem {
         key: "Format";
         value: string[];
     }
-    interface ASSSectionBodyStringDictionaryItem {
+    export interface ASSSectionBodyStringDictionaryItem {
         key: string;
         value: { [key: string]: string };
     }
